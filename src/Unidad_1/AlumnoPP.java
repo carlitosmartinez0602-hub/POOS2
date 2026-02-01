1315c936ef5ceae7ab503d4ee5337df2fd336425
alex <carlitosmartinez0602@gmail.com>
2026-01-31 19:07:22 -0600

    git add

---
+++ src/Unidad_1/AlumnoPP.java
@@ -0,0 +1,26 @@
+package Unidad_1;
+
+public class AlumnoPP {
+    // Atributos
+    int ID;
+    String Nombre;
+    String Apellido;
+
+    public void MostrarNombre(){
+        System.out.println("Hola, soy un alumno y sé decir mi nombre.");
+    }
+
+    public AlumnoPP(int ID, String nombre, String apellido) {
+        this.ID = ID;
+        Nombre = nombre;
+        Apellido = apellido;
+    }
+
+    public void SaberAprobado (double Nota){
+        if (Nota >= 6){
+            System.out.println("El Alumno sí aprobó.");
+        }else {
+            System.out.println("¡El alumno es un pendejo!");
+        }
+    }
+}
