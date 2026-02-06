373ab4dfdef776e940230fb5f7f52fd97ec2d309
zalork <carlitosmartinez0602@gmail.com>
2026-02-05 22:23:11 -0600

    git add

--- src/Unidad_1/Practica_1.java
+++ src/Unidad_1/Practica_1.java
@@ -1,41 +1,29 @@
 package Unidad_1;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Locale;
+
 public class Practica_1 {
     static void main() {
-        /*String V = "Hola, tilin";   // Objeto / Variable de referencia. Variable hecha de un tipo de dato de una clase.
-        V.chars(); // Verbo / Acción.
-        // Funciones de cadenas (Strings)
-        //V.toUpperCase(); // minisculas  a mayusculas.
-        String resultado;
-        resultado = V.toUpperCase();
-        System.out.println(resultado);
-        V.charAt(0);*/
-
-        String V = "Hola mundo";
+        String V = "Está erguido, ¡cual estatua romana! y así permanerá hasta" +
+                "que la muerte lo convierta en mármol";
         // Encuentra la posición de el parámetro del indexOF();
-        System.out.println(V.indexOf("mundo"));
+        System.out.println(V.indexOf("estatua"));
         // Encuentra la posición del parámetro del indexOF, buscando entre un index inicial y final. Busca un String
-        System.out.println(V.indexOf("mundo", 4, 10));
+        System.out.println(V.indexOf("así", 4, 50));
         // Encuentra la posición del parámetro del indexOF, buscando entre un index inicial y final. Busca un Caracter
-        System.out.println(V.indexOf("o", 3, 10));
+        System.out.println(V.indexOf("o", 3, 23));
         // Verifica si dentro de la cadena existe el o los caracteres que hay en los parámetros
-        System.out.println(V.contains("ola"));
-
-        // Buscar cantidad de letras (especificada)
-        char a = 'o';
-        int cont= 0;
-        int pos = 0;
-
-        do {
-            pos = V.indexOf(a, pos + 1);
-            if (pos!=-1){
-                System.out.println("posicion: " + cont + ": indice: "+ pos);
-                cont++;
-            }
-        }while (pos!=-1);
-
-        if (cont ==0){
-            System.out.println("no se encontraron letras " + a);
-        }
+        System.out.println(V.contains("marmol"));
+        // Devuelve el carácter en tipo char con el índice especificado
+        System.out.println(V.charAt(5));
+        // Hace que el texto de la cadena se vuelva mayuscula
+        System.out.println(V.toUpperCase(Locale.ROOT));
+        // Hace que el texto de la cadena sean todos minúsculas
+        System.out.println(V.toLowerCase(Locale.ROOT));
+        // Devuelve el resultado en forma de codigo ASCII (en este caso Alt + 115 = s)
+        System.out.println(V.codePointAt(1));
+        // Devuelve la longitud o tamaño de la cadena
+        System.out.println(V.length());
     }
 }
