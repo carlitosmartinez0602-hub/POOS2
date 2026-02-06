d1cd578d39cefbb06053450c9c81d0d9095f0f33
alex <carlitosmartinez0602@gmail.com>
2026-02-05 21:26:45 -0600

    git add

--- src/Unidad_1/Practica_2.java
+++ src/Unidad_1/Practica_2.java
@@ -2,6 +2,18 @@ package Unidad_1;
 
 public class Practica_2 {
     static void main() {
+        String V = "Está erguido, ¡cual estatua romana! y así permanerá hasta" +
+                "que la muerte lo convierta en mármol";
+        char a = 'a';
+        int pos = 0;
+        int cont = 1;
+        do {
+            pos = V.indexOf(a, pos +1);
+            if (pos != -1){
+                System.out.println("Posición: " + cont + " || Index: " + pos);
+                cont++;
+            }
 
+        }while(pos !=-1);
     }
 }
