4d90fbfe95a279da177c7b141554517bb320ce28
zalork <carlitosmartinez0602@gmail.com>
2026-02-25 23:31:47 -0600

    git add

--- src/Unidad_2/Practica_1/Practica_1_1.java
+++ src/Unidad_2/Practica_1/Practica_1_1.java
@@ -1,16 +1,23 @@
 package Unidad_2.Practica_1;
 
-import javax.swing.*;
-
 public class Practica_1_1 {
     public static class Matematicas {
-
-        public void factorial(int numero) {
+        long fn=0;
+        public long factorial(long numero) {
             long fctl = 1;
             for (long i = 1; i <= numero; i++) {
                 fctl *= i;
             }
+            return fctl;
         }
-
+        public long suma(long numero, long numero2 ){
+            fn = numero + numero2;
+            return fn;
+        }
+        public double logaritmoNatural(double numero){
+            double fn=0;
+            fn = Math.log(numero);
+            return fn;
+        }
     }
 }
--- src/Unidad_2/Practica_1/Principal.java
+++ src/Unidad_2/Practica_1/Principal.java
@@ -3,10 +3,53 @@ package Unidad_2.Practica_1;
 import javax.swing.*;
 
 public class Principal {
-    static void main() {
-        int n= Integer.parseInt(JOptionPane.showInputDialog(null, "Dame un número: "));
-        Practica_1_1.Matematicas mat = new Practica_1_1.Matematicas();
-        mat.factorial(n);
-        JOptionPane.showMessageDialog(null, "Resultado: " + mat);
+    public static void main(String[] args) {
+
+        long n=0;
+        long fn=0;
+        int el = Integer.parseInt(JOptionPane.showInputDialog(null,
+                "1.- Factorial\n" +
+                "2.- Suma\n" +
+                "3.- Logarimo\n" +
+                "Selecciona la opción"));
+
+
+        switch (el){
+            case 1:
+
+                if (n > 20) {
+                    JOptionPane.showMessageDialog(null, "Excede la memoria");
+                    return;
+                } else if (n < 0) {
+                    JOptionPane.showMessageDialog(null, "No se puede calcular el factorial" +
+                            " de un número negativo");
+                    return;
+                }
+
+                n=Long.parseLong(JOptionPane.showInputDialog(null, "Dame un número: "));
+                Practica_1_1.Matematicas facto = new Practica_1_1.Matematicas();
+                fn = facto.factorial(n);
+                JOptionPane.showMessageDialog(null, "Resultado: " + fn);
+
+                break;
+            case 2:
+                n=Long.parseLong(JOptionPane.showInputDialog(null, "Dame un número: "));
+                long n2 = Integer.parseInt(JOptionPane.showInputDialog(null, "Dame " +
+                        "un segundo número: "));
+                Practica_1_1.Matematicas sum = new Practica_1_1.Matematicas();
+                fn = sum.suma(n,n2);
+                JOptionPane.showMessageDialog(null, "Resultado: " + fn);
+
+                break;
+            case 3:
+                double fnlog=0;
+                n=Long.parseLong(JOptionPane.showInputDialog(null, "Dame un número: "));
+                Practica_1_1.Matematicas loga = new Practica_1_1.Matematicas();
+                fnlog =loga.logaritmoNatural(n);
+                JOptionPane.showMessageDialog(null, "Resultado: " + fnlog);
+                break;
+            default:
+                JOptionPane.showMessageDialog(null, "Opción no válida :(");
+        }
     }
 }
