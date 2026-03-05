e571a48632c150f274320479d0aade63f2aae0e8
alex <carlitosmartinez0602@gmail.com>
2026-03-04 23:22:06 -0600

    git commit

--- src/Unidad_2/Practica_3/Practica_3_1.java
+++ src/Unidad_2/Practica_3/Practica_3_1.java
@@ -1,14 +1,11 @@
 package Unidad_2.Practica_3;
 
 public class Practica_3_1 {
-    public static class temperatura{
-        double temp, gC, gF, fn;
+    public static class temperatura {
+        private double temp;
 
-        public temperatura(double temp, double gC, double gF, double fn) {
+        public temperatura() {
             this.temp = temp;
-            this.gC = gC;
-            this.gF = gF;
-            this.fn = fn;
         }
 
         public double getTemp() {
@@ -18,30 +15,11 @@ public class Practica_3_1 {
         public void setTemp(double temp) {
             this.temp = temp;
         }
-
-        public double getgC() {
-
-            return gC;
+        public double getgCK(){
+            return temp + 32;
         }
-
-        public void setgC(double gC) {
-            this.gC = gC;
-        }
-
-        public double getgF() {
-            return gF;
-        }
-
-        public void setgF(double gF) {
-            this.gF = gF;
-        }
-
-        public double getFn() {
-            return fn;
-        }
-
-        public void setFn(double fn) {
-            this.fn = fn;
+        public double getgCF(){
+            return (temp * 9/5) +32;
         }
     }
 }
--- src/Unidad_2/Practica_3/Principal_3.java
+++ src/Unidad_2/Practica_3/Principal_3.java
@@ -1,7 +1,51 @@
 package Unidad_2.Practica_3;
 
+import javax.swing.*;
+
 public class Principal_3 {
     static void main(String[] args) {
+        int el=0;
+        double temCel =0;
 
+        Practica_3_1.temperatura temp = new Practica_3_1.temperatura();
+        do{
+            el = Integer.parseInt(JOptionPane.showInputDialog("Opciones:\n\n" +
+                    "0.- Salir\n" +
+                    "1.- Alta de temperatura\n" +
+                    "2.- Celsius a Fahrenheit\n" +
+                    "3.- Celsius a Kelvin\n\n" +
+                    "Selecciona la conversión"));
+
+            switch(el){
+                case 0:
+                    JOptionPane.showMessageDialog(null, "Ten un buen día :)");
+                    return;
+                case 1:
+                    temCel = Double.parseDouble(JOptionPane.showInputDialog("Grados celsius: "));
+                    temp.setTemp(temCel);
+                    JOptionPane.showMessageDialog(null,"Temperatura: " + temCel + " °C");
+                    break;
+                case 2:
+                    if (temp.getTemp() == 0 && temCel == 0) {
+                        JOptionPane.showMessageDialog(null,
+                                "Primero debe dar de alta una temperatura (opción 1)");
+                    } else {
+                        JOptionPane.showMessageDialog(null,
+                                "La temperatura: " + temp.getTemp() + "°C\n" + "en grados Fahrenheit es: " + temp.getgCF() + "°F");
+                    }
+                    break;
+                case 3:
+                    if (temp.getTemp() == 0 && temCel == 0) {
+                        JOptionPane.showMessageDialog(null,
+                                "Primero debe dar de alta una temperatura (opción 1)");
+                    } else {
+                        JOptionPane.showMessageDialog(null,
+                                "La temperatura: " + temp.getTemp() + "°C\n" + "en grados Kelvin es: " + temp.getgCK() + "°K");
+                    }
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Opción no válida");
+            }
+        }while(el != 4);
     }
 }
