5b8cd978ecd895fca8750b2af789aaf47b670fe0
zalork <carlitosmartinez0602@gmail.com>
2026-04-13 17:48:28 -0600

    git commit

--- src/Unidad_2/Practica_20/Practica_20.java
+++ src/Unidad_2/Practica_20/Practica_20.java
@@ -1,13 +1,13 @@
 package Unidad_2.Practica_20;
 
 import javax.swing.*;
-import java.util.Arrays;
 
 public class Practica_20 {
     static void main(String[] args) {
         //Pedir al usuario una cadena
         String cad = pDat("Lista de compra: ");
-        int cont=0, caden=0;
+        int cont=0;
+        String caden="";
         //Separar las palabras de la cadena ademas de espaciarlas un renglón
         String cade = String.join("\n", cad.split(","));
         //Contar la cantidad de letras que hay en una cadena
@@ -15,23 +15,21 @@ public class Practica_20 {
             char c  = cade.charAt(i);
             if (Character.isLetter(c)){
                 cont++;
+                caden += c;
             }
             if (c == '\n') {
                 dInf(caden + " - " + cont);
-                caden++;
                 cont=0;
+                caden="";
             }
         }
         //Mostrar info
         String fn = " ";
-        fn += cade +" - "+ cont;
+        fn += caden + " - " + cont;
         dInf(fn);
     }
     public static String pDat(String txt){
         return JOptionPane.showInputDialog(txt);
     }
-    public static String dInf(String txt){
-        JOptionPane.showMessageDialog(null, txt);
-        return "";
-    }
+    public static String dInf(String txt){JOptionPane.showMessageDialog(null, txt);return ""; }
 }
